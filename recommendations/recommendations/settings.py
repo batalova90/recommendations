c31fc904ec5ace9dd376b21b8dc4dffe4726a9cf
Batalova Anastasiya <batalova.av@gmail.com>
2021-11-27 17:34:06 +0300

    deploy heroku

--- recommendations/recommendations/settings.py
+++ recommendations/recommendations/settings.py
@@ -117,8 +117,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIR = (
-    # os.path.join(PROJECT_ROOT, 'static')
-# )
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
