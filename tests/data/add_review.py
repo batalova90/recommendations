e4a08c5c952a854c35593b14f12ad99658084889
Batalova Anastasiya <batalova.av@gmail.com>
2022-05-31 16:23:34 +0300

    added data

---
+++ tests/data/add_review.py
@@ -0,0 +1,31 @@
+testdata = [
+    {"name":  "Test review",
+     "rating": "1",
+     "slug": "test1",
+     "text": "Check test",
+     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/solyanka.jpg",
+     "review_name": "Test 1"},
+    
+    {"name": "Test review 2",
+     "rating": "1",
+     "slug": "test2",
+     "text": "Check test",
+     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/venom.jpg",
+     "review_name": "Test 2"},
+
+    {"name": "Test review 3",
+     "rating": "1",
+     "slug": "test3",
+     "text": "Check test",
+     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/idiot.jpg",
+     "review_name": "Test 3"},
+
+    {"name": "Test review 4",
+     "rating": "1",
+     "slug": "test4",
+     "text": "Check test",
+     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/maer.jpg",
+     "review_name": "Test 4"}
+
+]
+
--- tests/review/test_create_review.py
+++ tests/review/test_create_review.py
@@ -1,39 +1,9 @@
 import pytest
 from selenium.webdriver.common.by import By
 
+from data.add_review import testdata
 
 # надо удалять обзоры
-testdata = [
-    {"name":  "Test review",
-     "rating": "4",
-     "slug": "test1",
-     "text": "Check test",
-     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/solyanka.jpg",
-     "review_name": "Test 1"},
-    
-    {"name": "Test review 2",
-     "rating": "3",
-     "slug": "test2",
-     "text": "Check test",
-     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/venom.jpg",
-     "review_name": "Test 2"},
-
-    {"name": "Test review 3",
-     "rating": "2",
-     "slug": "test3",
-     "text": "Check test",
-     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/idiot.jpg",
-     "review_name": "Test 3"},
-
-    {"name": "Test review 4",
-     "rating": "1",
-     "slug": "test4",
-     "text": "Check test",
-     "path_image": "/home/anastasiya/ittransition/recommendations/tests/review/image/maer.jpg",
-     "review_name": "Test 4"}
-
-]
-
 
 @pytest.mark.parametrize("review", testdata, ids=[repr(x) for x in testdata])
 def test_create_review(app, review):
